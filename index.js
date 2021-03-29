c14e4dba282ff83227f29efccc8de00adee4aa4f
Rifat Al- Ashwad <rifatbilalphilips@gmail.com>
2021-03-29 17:02:21 +0600

    basic store

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
+const redux = require('redux');
+ const createStore = redux.createStore;
 const BUY_CAKE = 'BUY_CAKE';
-const buyCake = () => {
+function buyCake() {
     return  {
         type: BUY_CAKE,
         info:'first reduc action'
@@ -18,3 +20,11 @@ const reducer = (state = initialState,action) => {
     default: return state
   }
 }
+
+const store = createStore(reducer)
+console.log('initial state', store.getState());
+const unsubscribe = store.subscribe(() => console.log('update state', store.getState()));
+store.dispatch(buyCake())
+store.dispatch(buyCake())
+store.dispatch(buyCake())
+unsubscribe()
