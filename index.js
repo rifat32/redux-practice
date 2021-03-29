9fdfd4e3ea51c2597c0e0dc6e3f164faf617af20
Rifat Al- Ashwad <rifatbilalphilips@gmail.com>
2021-03-29 16:17:17 +0600

    basic action and reducer

---
+++ index.js
@@ -0,0 +1,20 @@
+const BUY_CAKE = 'BUY_CAKE';
+const buyCake = () => {
+    return  {
+        type: BUY_CAKE,
+        info:'first reduc action'
+    }
+} 
+const initialState = {
+    numOfCakes: 10
+}
+const reducer = (state = initialState,action) => {
+  switch(action.type) {
+    case BUY_CAKE: 
+    return {
+        ...state,
+        numOfCakes:state.numOfCakes - 1
+    }
+    default: return state
+  }
+}
